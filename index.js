6feb221bc345f47ecc218c0e27dedef027937ce1
CarlosEduardo214 <cers.405@gmail.com>
2025-01-21 20:29:22 -0300

    programando

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 let nomeDoHeroi = ("Carlos");
-let quantidadeDeXp = ("800");
+let quantidadeDeXp = ("2600");
  
-if (quantidadeDeXp < 1000) {
+if (quantidadeDeXp <= 1000) {
     console.log("O Herói de nome " + nomeDoHeroi + " está no nível de ferro");
 }
+else if (quantidadeDeXp >= 1001 && quantidadeDeXp <= 2000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de bronze");
+}
+else if (quantidadeDeXp >= 2001 && quantidadeDeXp <= 5000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de prata");
+}
+else if (quantidadeDeXp >= 5001 && quantidadeDeXp <= 7000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de ouro");
+}
+
+
