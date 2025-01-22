4aa9ec8710ad3b73679d56349b34307259b12d9c
CarlosEduardo214 <cers.405@gmail.com>
2025-01-22 20:41:34 -0300

    Etudando

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let nomeDoHeroi = ("Carlos");
-let quantidadeDeXp = ("2600");
+let nomeDoHeroi = ("Carlos Eduardo");
+let quantidadeDeXp = ("7300");
  
 if (quantidadeDeXp <= 1000) {
     console.log("O Herói de nome " + nomeDoHeroi + " está no nível de ferro");
@@ -13,5 +13,15 @@ else if (quantidadeDeXp >= 2001 && quantidadeDeXp <= 5000) {
 else if (quantidadeDeXp >= 5001 && quantidadeDeXp <= 7000) {
     console.log("O Herói de nome " + nomeDoHeroi + " está no nível de ouro");
 }
-
-
+else if (quantidadeDeXp >= 7001 && quantidadeDeXp <= 8000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de platina");
+}
+else if (quantidadeDeXp >= 8001 && quantidadeDeXp <= 9000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ascendente");
+}
+else if (quantidadeDeXp >= 9001 && quantidadeDeXp <= 10000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Imortal");
+}
+else if (quantidadeDeXp >= 10001) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Radiante");
+}
